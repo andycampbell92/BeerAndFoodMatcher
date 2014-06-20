f5cb73345fd4c49eeb7b7b00e581748f35fd0720
andycampbell92 <andycampbell92@gmail.com>
2014-06-20 20:29:43 +0100

    Got edge nodes drawing without positioning

--- main.js
+++ main.js
@@ -6,37 +6,60 @@ var ready = function(){
 	 	.attr("class", "canvas")
 	 	.attr("id", "ourCanvas");
 
-	 var clearSVG = function(){
+	 var resetDrawing = function(){
 	 	$('#ourCanvas').empty();
+	 	window.drawnData = null;
 	 }
 
 	 var drawHighLevel = function(svg){
 	 	var drawColumn = function(svg, data){
-	 		var superNodes = [];
+	 		var superNodes = {};
 	 		var canvasWidth = $('#ourCanvas').width();
 	 		var rectDims = {width:250, height:20};
 	 		var margin = 30;
 	 		data.forEach(function(node){
-	 			if(node.superNode === true){
-	 				var newNode = svg.append("g")
-	 						.attr("transform", "translate(" + (canvasWidth/2 - rectDims.width/2) + "," + (rectDims.height*(superNodes.length+1)) + ")");
+ 				superNodes[node._id] = {data:node};
+ 				var newNode = svg.append("g")
+ 						.attr("transform", "translate(" + (canvasWidth/2 - rectDims.width/2) + "," + (rectDims.height*(Object.keys(superNodes).length+1)) + ")");
 
-                    newNode.append("rect")
-                    	   .attr("width", rectDims.width)
-                           .attr("height", rectDims.height)
-                           .attr("class", "centre-column");
+                newNode.append("rect")
+                	   .attr("width", rectDims.width)
+                       .attr("height", rectDims.height)
+                       .attr("class", "centre-column");
 
-                    newNode.append("text")
-                    		.text(function(d){return node.name})
-                    		.attr("class", "centre-column")
-                    		.attr("transform", "translate(15,15)");
+                newNode.append("text")
+                		.text(function(d){return node.name})
+                		.attr("class", "centre-column")
+                		.attr("transform", "translate(15,15)");
 
-                    superNodes.push(newNode);
-	 			};
-	 		})
+                superNodes[node._id].element = newNode;
+	 		});
+	 		return superNodes
 	 	}
-	 	clearSVG();
-	 	api.getHighLevelData().then(function(data){drawColumn(this.svg, data)});
+	 	var drawEdgeNodes = function(svg, data, columnData){
+	 		data.forEach(function(superNode){
+	 			superNode.links.forEach(function(edgeNode){
+	 				columnData[edgeNode._id] = {data:edgeNode};
+	 				var newNode = svg.append("g")
+	 					.attr("transform", "translate(10, 20)");
+	 				newNode.append("circle")
+	 					.attr("r", 40)
+	 					.attr("class", "edge-node");
+
+	 				newNode.append("text")
+	 					.text(function(d){return edgeNode.name})
+	 					.attr("class", "edge-node");
+	 				columnData[edgeNode._id].element = newNode;
+	 			});
+	 		});
+	 		return columnData;
+	 	};
+	 	resetDrawing();
+	 	api.getHighLevelData().then(function(data){
+	 		var columnData = drawColumn(this.svg, data);
+	 		console.log(columnData);
+	 		window.drawnData = drawEdgeNodes(this.svg, data, columnData);
+	 	});
 	 }
 	 drawHighLevel();
 }
